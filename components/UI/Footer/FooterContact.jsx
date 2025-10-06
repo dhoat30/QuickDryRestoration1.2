e241d774caea1324c748afa0c3ccdbc5934a64b8
dhoat30 <dhoat30@gmail.com>
2025-10-06 21:31:38 +1300

    first commit

---
+++ components/UI/Footer/FooterContact.jsx
@@ -0,0 +1,28 @@
+export const contactInfo = { 
+    info:[ 
+        {
+            url: "tel:099711101", 
+            label: "09 971 1101",
+            icon: { 
+                url: "/phone.png", 
+                alt: "phone icon" 
+            },
+        },
+        {
+            url: "mailto:info@quickdryrestorations.co.nz", 
+            label: "info@quickdryrestorations.co.nz", 
+            icon: { 
+                url: "/email.png", 
+                alt: "Email icon" 
+            },
+        }, 
+           {
+            url: "/", 
+            label: "13 Westglen Place Ranui, Auckland 0612", 
+            icon: { 
+                url: "/map.png", 
+                alt: "Map icon" 
+            },
+        }
+    ]
+}
